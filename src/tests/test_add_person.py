b4c07a6085fa7aa2866d48ad07def5562961ccf6
esirK <esirkings@gmail.com>
2017-05-21 17:22:36 +0300

    Create a room before adding a person

--- src/tests/test_add_person.py
+++ src/tests/test_add_person.py
@@ -13,12 +13,14 @@ class TestAddPerson(unittest.TestCase):
                          + len(self.dojo.list_of_fellows))
 
     def test_add_person_adds_to_appropriate_container(self):
+        self.dojo.create_room("Makuti", "LivingSpace")
         self.dojo.add_person("Esir", "Fellow", "101", "Y")
         self.dojo.add_person("King'ori", "Staff", "11")
         self.assertEqual(len(self.dojo.list_of_fellows), 1)
         self.assertEqual(len(self.dojo.list_of_staffs), 1)
 
     def test_person_with_same_id_raises_user_exists_exception(self):
+        self.dojo.create_room("Makutile", "LivingSpace")
         with self.assertRaises(UserAlreadyExist):
             self.dojo.add_person("Wanjira", "Fellow", "999")
             self.dojo.add_person("Ngaruiya", "Fellow", "999", "Y")
