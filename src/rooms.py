3b5b740767c1485b8340429c573299a623d46885
esirK <esirkings@gmail.com>
2017-05-21 10:29:20 +0300

    Create add_person and add_fellow to Office class and LivingSpace respectively

--- src/rooms.py
+++ src/rooms.py
@@ -1,16 +1,36 @@
+from src.Exceptions.room_exceptions import OfficeFull, LivingSpaceFull
+
+
 class Room(object):
-    def __init__(self, name, room_type):
+    def __init__(self, name, room_type, capacity):
         self.name = name
         self.type = room_type
+        self.capacity = capacity
 
 
 class Office(Room):
     def __init__(self, name):
-        super(Office, self).__init__(name, "Office")
-        self.office_size = 6
+        super(Office, self).__init__(name, "Office", 6)
+        self.__persons = []
+
+    def add_person(self, person):
+        if len(self.__persons) > 6:
+            raise OfficeFull
+        else:
+            self.__persons.append(person)
+
+    def get_allocated_space(self):
+        # Return how many people have been allocated into this room
+        return len(self.__persons)
 
 
 class LivingSpace(Room):
     def __init__(self, name):
-        super(LivingSpace, self).__init__(name, "LivingSpace")
-        self.living_space_size = 4
+        super(LivingSpace, self).__init__(name, "LivingSpace", 4)
+        self.__fellows = []
+
+    def add_fellow(self, fellow):
+        if len(self.__fellows) > 4:
+            raise LivingSpaceFull
+        else:
+            self.__fellows.append(fellow)
